8c61abfe533f08315097160375a3988d1006b7f5
Samuel Girma Megra <samigirma53@gmail.com>
2022-11-11 01:52:49 +0300

    Create maxPoints.py

---
+++ maxPoints.py
@@ -0,0 +1,17 @@
+https://github.com/Sam-Girma/competitive-programing/blob/main/divide_two_integers.py
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points)==1:
+            return 1
+        max_points = 0
+        for point in points:
+            slopes = defaultdict(int)
+            for point2 in points:
+                if point == point2:
+                    continue
+                if point2[0]==point[0]:
+                    slopes["vert"]+=1
+                else:
+                    slopes[(point[1]-point2[1])/(point[0]-point2[0])]+=1
+            max_points = max(max_points, 1+max(slopes.values()))
+        return max_points
